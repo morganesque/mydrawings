3be845f0a3aeda8cef398e4aa56ed08c2ccfecc9
Tom Morgan <tommorgan.digitaldwp@gmail.com>
2016-12-04 20:09:22 +0000

    tidying the data because CSV isn't necessarily the best way to go when you data already has commas in it. *sigh*

--- makeAll.js
+++ makeAll.js
@@ -1,14 +1,26 @@
 var fs = require("fs");
+var moment = require("moment");
+var csv = require("csv");
 
-for (var i = 1; i < 164; i++) {
-  var out = '';
-  out += "---\n";
-  out += "layout: drawing_page\n";
-  out += "number: "+i+"\n";
-  out += "permalink: "+i+"/\n";
-  out += "---\n";
+var values = fs.readFileSync('_data/drawings.csv','utf8');
+csv.parse(values,function(err,data)
+{
+  for (var i = 1; i < data.length; i++) {
+    var timet = data[i][3];
+    var date = new moment(timet*1000,"x");
+    console.log(date);
+  }
+});
 
-  var file = 'drawings/'+i+".html";
-
-  fs.writeFileSync(file, out);
-}
+// for (var i = 1; i < 164; i++) {
+//   var out = '';
+//   out += "---\n";
+//   out += "layout: drawing_page\n";
+//   out += "number: "+i+"\n";
+//   out += "permalink: "+i+"/\n";
+//   out += "---\n";
+// 
+//   var file = 'drawings/'+i+".html";
+//
+//   fs.writeFileSync(file, out);
+// }
